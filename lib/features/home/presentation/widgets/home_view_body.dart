bbca0674e64e7c650a37a2f47f116b40eb16168c
00Abanoub <abanoubhabib2002@gmail.com>
2026-03-14 01:56:25 +0200

    phase one best seller list

--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../../core/utils/assets.dart';
 import '../../../../core/utils/styles.dart';
 import 'custom_app_bar.dart';
 import 'package:gap/gap.dart';
@@ -19,7 +20,40 @@ class HomeViewBody extends StatelessWidget {
         FeaturedBookHorizontalListView(),
         Gap(30),
         Text('Best Seller', style: Styles.textStyle20),
+        Gap(20),
+        BestSellerListViewItem(),
       ],
     );
   }
 }
+
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      height: 150,
+      child: Row(
+        children: [
+        AspectRatio(
+        //width:height
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(8),
+            image: DecorationImage(
+              image: AssetImage(AssetsData.soloLevelingImage),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      )
+        ],
+      ),
+    );
+  }
+}
+
